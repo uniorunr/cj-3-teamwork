6b778ad261f7d9e630ff2bfa4e73d990203faf68
Frol8612 <infern00730@gmail.com>
2019-01-25 17:04:25 +0300

    feat: add component tests and snapshots

---
+++ src/components/container/NavBar/NavBar.spec.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+import NavBar from './NavBar';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <MemoryRouter>
+        <NavBar />
+      </MemoryRouter>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
