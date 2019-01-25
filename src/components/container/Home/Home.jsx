702b350a0c3a5b94493b0c263f617a2eeac2171a
Frol8612 <infern00730@gmail.com>
2019-01-25 12:36:39 +0300

    feat: add Home component

---
+++ src/components/container/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Home.scss';
+
+const Home = () => (
+  <div className="main-head">
+    <h2>&ldquo;Directors of the Theater of Belarus&bdquo;</h2>
+  </div>
+);
+
+export default Home;
