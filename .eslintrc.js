6c8eff228f60d28608b7b62dcf37a791b0c48d7e
Frol8612 <infern00730@gmail.com>
2019-01-25 12:29:35 +0300

    feat: add config file

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: 'airbnb',
+  env: {
+    browser: true,
+  },
+  plugins: ['react', 'lodash', 'jsx-a11y', 'import'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  parser: 'babel-eslint',
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+};
