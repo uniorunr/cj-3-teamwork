e1d74f772db3f40ec1041dfeac21c44dfcbbdb5f
Frol8612 <infern00730@gmail.com>
2019-01-25 17:01:53 +0300

    feat: add import normalize

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import 'normalize.css';
 import './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
