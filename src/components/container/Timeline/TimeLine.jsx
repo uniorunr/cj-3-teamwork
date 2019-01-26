0e0eb70894cdf11b2f34a5297a2d2f2536ea599a
uniorunr <gagael23@live.com>
2019-01-26 23:46:36 +0300

    feature: add timeline component

---
+++ src/components/container/Timeline/TimeLine.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
+import PropTypes from 'prop-types';
+
+const TimeLine = ({ content }) => (
+  <Timeline lineColor="#ddd">
+    {
+        content.items.map(item => (
+          <TimelineItem
+            key={`${item.date}`}
+            dateInnerStyle={{ background: '#000000', color: '#ffffff' }}
+            dateText={`${item.date.charAt(0).toLowerCase()}${item.date.slice(1)}`}
+            style={{ color: 'black' }}
+          >
+            <h3>{`${item.title.charAt(0).toUpperCase()}${item.title.slice(1)}`}</h3>
+          </TimelineItem>
+        ))
+    }
+  </Timeline>
+);
+
+TimeLine.propTypes = {
+  content: PropTypes.instanceOf(Object).isRequired,
+};
+
+export default TimeLine;
