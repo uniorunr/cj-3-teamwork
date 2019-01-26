4eb479f787f3c7777c4144707e023d99b727d9b6
Frol8612 <infern00730@gmail.com>
2019-01-26 19:23:39 +0300

    feat: change file

--- src/components/container/NavBar/NavBar.jsx
+++ src/components/container/NavBar/NavBar.jsx
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import NavBarItem from '../../presentetion/NavBarItem/NavBarItem';
+import ru from '../../../config/ru.json';
 import './NavBar.css';
 
-const NavBar = () => (
-  <div className="navbar-container">
-    <div className="header-logo">
-      <h1>Director</h1>
-    </div>
-    <nav>
-      <ul>
-        <NavBarItem hash="/" name="Home" />
-        <NavBarItem hash="/about" name="About" />
-        <NavBarItem hash="/users" name="Users" />
-      </ul>
-    </nav>
-  </div>
-);
+class NavBar extends Component {
+  state = {
+    active: true,
+    id: 'home',
+  };
+
+  handlerClickActive = (e) => {
+    const { active, id } = this.state;
+    const targetId = e.target.id;
+    if (targetId !== id) {
+      this.setState({
+        active: !active,
+        id: targetId,
+      });
+    }
+  };
+
+  render() {
+    const { active } = this.state;
+    return (
+      <div className="navbar-container">
+        <nav>
+          <ul>
+            <NavBarItem
+              id="home"
+              active={active ? 'active' : 'not-active'}
+              hash="/"
+              name={ru.navbar.home}
+              onClick={this.handlerClickActive}
+            />
+            <NavBarItem
+              id="list"
+              active={active ? 'not-active' : 'active'}
+              hash="/list"
+              name={ru.navbar.list}
+              onClick={this.handlerClickActive}
+            />
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
 
 export default NavBar;
