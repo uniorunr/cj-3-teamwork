eb4ed48b0734eb5692dc8896d9810b8127bf000d
Frol8612 <infern00730@gmail.com>
2019-01-26 19:22:32 +0300

    feat: change file

--- src/components/presentetion/NavBarItem/NavBarItem.jsx
+++ src/components/presentetion/NavBarItem/NavBarItem.jsx
@@ -3,15 +3,20 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './NavBarItem.css';
 
-const NavBarItem = ({ hash, name }) => (
-  <li>
-    <Link to={hash}>{name}</Link>
+const NavBarItem = ({
+  id, active, hash, name, onClick,
+}) => (
+  <li className={active}>
+    <Link id={id} to={hash} onClick={onClick}>{name}</Link>
   </li>
 );
 
 NavBarItem.propTypes = {
+  id: PropTypes.string.isRequired,
   hash: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  active: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default NavBarItem;
