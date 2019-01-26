75eea7018dedb6dd2ab965f43c2d29ee454dd4e0
Frol8612 <infern00730@gmail.com>
2019-01-27 01:01:32 +0300

    feat: update test

--- src/components/container/Home/Home.spec.js
+++ src/components/container/Home/Home.spec.js
@@ -3,8 +3,6 @@ import renderer from 'react-test-renderer';
 import Home from './Home';
 
 it('renders correctly', () => {
-  const tree = renderer
-    .create(<Home />)
-    .toJSON();
+  const tree = renderer.create(<Home teams={[{ name: 'a', img: 'img1' }, { name: 'b', img: 'img2' }]} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
