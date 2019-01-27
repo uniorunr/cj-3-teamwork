1e57a4a0024e59a498c4a3b3b5ce39b2c2c1c53c
uniorunr <gagael23@live.com>
2019-01-27 23:16:25 +0300

    #1/feature: add VideoOverlay component to the project

---
+++ src/components/container/ListDirectors/VideoOverlay/VideoOverlay.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import ModalVideo from 'react-modal-video';
+import PropTypes from 'prop-types';
+import 'react-modal-video/css/modal-video.min.css';
+import './VideoOverlay.css';
+
+class VideoOverlay extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isOpen: false,
+    };
+    this.openModal = this.openModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({ isOpen: true });
+  }
+
+  render() {
+    const { video } = this.props;
+    const { isOpen } = this.state;
+    return (
+      <div className="video-overlay-wrapper">
+        <ModalVideo
+          channel="youtube"
+          isOpen={isOpen}
+          videoId={video[0]}
+          onClose={() => this.setState({ isOpen: false })}
+        />
+        <button className="video-play-button" type="button" onClick={this.openModal}>Просмотреть видео</button>
+      </div>
+    );
+  }
+}
+
+VideoOverlay.propTypes = {
+  video: PropTypes.arrayOf(
+    PropTypes.string,
+  ),
+};
+
+VideoOverlay.defaultProps = {
+  video: 'video',
+};
+
+export default VideoOverlay;
