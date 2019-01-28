0d01779522cea9723ac08874c44b453c8bcc51c6
uniorunr <gagael23@live.com>
2019-01-29 00:27:35 +0300

    deploy website

--- src/components/container/NavBar/NavBar.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import NavBar from './NavBar';
-import ru from '../../../config/ru.json';
-
-it('renders correctly', () => {
-  const tree = renderer.create(<NavBar lang={ru} active onClick={() => {}} />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
