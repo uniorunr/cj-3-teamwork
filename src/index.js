b4736c05811d5bda0a8f28c715f5ff39eb57931a
Frol8612 <infern00730@gmail.com>
2019-01-28 23:44:56 +0300

    refactor: update file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'normalize.css';
 import './index.css';
-import './fix.htaccess';
 
 ReactDOM.render(<App />, document.getElementById('root'));
